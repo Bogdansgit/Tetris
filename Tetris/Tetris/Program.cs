bdedecfa18db164f8028eb1d8f7b5fa1afaed0a2
Bogdan <Bogdanemails@gmail.com>
2020-11-22 22:13:39 +0200

    Фігурку рухаємо самі. Фігурка stick повертається.

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 
 namespace Tetris
 {
@@ -6,7 +7,39 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(Field.Width, Field.HEIGHT);
+            Console.SetBufferSize(Field.Width, Field.HEIGHT);
+
+            FigureGenerator generator = new FigureGenerator(20, 0, '*');
+            Figure currentFigure = generator.GetNewFigure();
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey();
+                    HandleKey(currentFigure, key);
+                }
+            }
+        }
+
+        private static void HandleKey(Figure currentFigure, ConsoleKeyInfo key)
+        {
+            switch (key.Key)
+            {
+                case ConsoleKey.LeftArrow:
+                    currentFigure.TryMove(Direction.LEFT);
+                    break;
+                case ConsoleKey.RightArrow:
+                    currentFigure.TryMove(Direction.RIGHT);
+                    break;
+                case ConsoleKey.DownArrow:
+                    currentFigure.TryMove(Direction.DOWN);
+                    break;
+                case ConsoleKey.Spacebar:
+                    currentFigure.TryRotate();
+                    break;
+            }
         }
     }
 }
